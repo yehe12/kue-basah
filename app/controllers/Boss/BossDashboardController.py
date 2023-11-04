8e81e887106650cc86781bb03af63d1e23266396
muhammad abdullah <ma22052000@gmail.com>
2023-11-05 03:44:44 +0700

    hapus yang tidak dibutuhkan dan update gitignore

--- app/controllers/Boss/BossDashboardController.py
+++ app/controllers/Boss/BossDashboardController.py
@@ -2,7 +2,6 @@ from app import app
 from flask import session, render_template, redirect, url_for
 
 from app.models.user import *
-from app.models.repair import *
 
 @app.route('/boss/dashboard')
 def bossDashboard():
@@ -10,24 +9,8 @@ def bossDashboard():
         if session['role'] == 1 :
 
             count_users = User().countUser()
-            repairTaken = Repair().countRepairTaken()
-            count_cost_totals = Repair().countCostTotal()
-            count_repairs = Repair().countRepair()
-            count_in_repairs = Repair().countInRepair()
 
-            taken =  int(repairTaken[0])
-            
-            data_cost_total = 0
-            if taken != 0:    
-                data_cost_total =  int(count_cost_totals[0])
-
-            data_repair = str(count_repairs[0]).strip("()'")
-
-            data_in_repair = str(count_in_repairs[0]).strip("()'")
-
-            data_user = str(count_users[0]).strip("()'")
-
-            return render_template('boss/boss_dashboard.html', count_user=data_user, count_cost_total=data_cost_total, count_repair=data_repair, count_in_repair=data_in_repair)
+            return render_template('boss/boss_dashboard.html', count_user=data_user, count_cost_total=data_cost_total)
 
         else :
 
--- app/controllers/bossController.py
+++ app/controllers/bossController.py
@@ -1,8 +1,6 @@
 from app import app
 from app.config.db import *
 from app.models.user import *
-from app.models.repair import *
-from app.models.repair_detail import *
 
 from flask import Flask, render_template, url_for, redirect, session, request
 from flaskext.mysql import MySQL
@@ -34,38 +32,6 @@ def bossUserDetail(id):
             role = User().selectRole()
 
             return render_template('bos/user_detail.html', user_details=user_detail, roles=role)
-
-        else :
-            return redirect(url_for('dashboard'))
-
-    return redirect(url_for('login'))
-
-#fungsi melihat perbaikan
-@app.route('/boss/repair')
-def bossRepair():
-
-    if 'loggedin' in session:
-        if session['role'] == 3 :
-            
-            repair = Repair().selectRepair()
-
-            return render_template('bos/repair.html', repairs=repair)
-
-        else :
-            return redirect(url_for('dashboard'))
-
-    return redirect(url_for('login'))
-
-@app.route('/boss/repair/detail/<int:id>')
-def bossRepairDetail(id):
-
-    if 'loggedin' in session:
-        if session['role'] == 3 :
-            
-            repair_detail = RepairDetail().selectRepairDetail(id)
-            repair_one = Repair().selectRepairOne(id)
-
-            return render_template('bos/repair_detail.html',repair_ones=repair_one, repair_details=repair_detail, repair_id=id)
 
         else :
             return redirect(url_for('dashboard'))
