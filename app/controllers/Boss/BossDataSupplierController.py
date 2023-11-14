59bf11e0cebe9c8f4b40e59cf12261a810fea1ff
muhammad abdullah <ma22052000@gmail.com>
2023-11-15 06:37:12 +0700

    add list data supplier

--- app/controllers/Boss/BossDataSupplierController.py
+++ app/controllers/Boss/BossDataSupplierController.py
@@ -1,4 +1,5 @@
 from app import app
+from app.models.user import *
 from flask import session, render_template, redirect, url_for
 
 # from app.models.user import *
@@ -7,9 +8,10 @@ from flask import session, render_template, redirect, url_for
 def bossDataSupplier():
     if 'loggedin' in session:
         if session['role'] == 1 :
-            # ini fungsi lain di dalam controller ini
+            
+            getSupplier = User().selectSupplier()
 
-            return render_template('boss/boss_data_supplier.html')
+            return render_template('boss/boss_data_supplier.html', dataSupplier=getSupplier)
         else :
             return redirect(url_for('dashboard'))
         
