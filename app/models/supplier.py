d0091c215a2c6c3bb355bf508ac9ac74389e1d07
muhammad abdullah <ma22052000@gmail.com>
2023-12-10 08:52:44 +0700

    tambah fungsi add, edit data supplier

--- app/models/supplier.py
+++ app/models/supplier.py
@@ -7,9 +7,9 @@ class Supplier:
     def selectSupplier(self):
         
         cursor = mysql.get_db().cursor()
-        dataSupplier = "SELECT * FROM supplier;"
+        select_query = "SELECT * FROM supplier ORDER BY create_at DESC;"
         # dataSupplier = "SELECT nama_suplier, umur FROM supplier;"
-        cursor.execute(dataSupplier)
+        cursor.execute(select_query)
         dataSupplier = cursor.fetchall()
 
         return dataSupplier
@@ -25,5 +25,28 @@ class Supplier:
         insert_query = "INSERT INTO supplier (nama_suplier, nomor_telepon, alamat, status, create_at) VALUES (%s, %s, %s, %s, now())"
         cursor.execute(insert_query, (self.nama_suplier, self.nomor_telepon, self.alamat, self.status))
         mysql.get_db().commit()
+        cursor.close()
+        
+    def selectSupplierOne(self, id):
+        self.id = id
+        cursor = mysql.get_db().cursor()
+        select_one = "SELECT * from supplier WHERE id=%s"
+        cursor.execute(select_one, (self.id))
+        
+        dataSupplierOne = cursor.fetchone()
 
+        return dataSupplierOne
+
+    def updateSupplier(self, id, nama_suplier, nomor_telepon, status, alamat):
+
+        self.id = id
+        self.nama_suplier = nama_suplier
+        self.nomor_telepon = nomor_telepon
+        self.status = status
+        self.alamat = alamat
+
+        cursor = mysql.get_db().cursor()
+        update_query = "UPDATE supplier SET nama_suplier = %s, nomor_telepon = %s, status = %s, alamat = %s WHERE id = %s"
+        cursor.execute(update_query, (self.nama_suplier, self.nomor_telepon, self.status, self.alamat, self.id))
+        mysql.get_db().commit()
         cursor.close()
