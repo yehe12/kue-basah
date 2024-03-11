e966327c4269755d5b246b24bfe7dcc6eae1e3c8
muhammad abdullah <ma22052000@gmail.com>
2024-03-11 12:12:14 +0700

    pengiriman shop keeper

--- app/controllers/Boss/BossDataPengirimanController.py
+++ app/controllers/Boss/BossDataPengirimanController.py
@@ -7,7 +7,7 @@ from flask import session, render_template, redirect, url_for, request
 @app.route('/data-pengiriman')
 def bossDataPengiriman():
     if 'loggedin' in session:
-        if session['role'] == 1 :
+        if session['role'] == 1 or session['role'] == 2 :
             
             getPengiriman = Pengiriman().selectPengiriman()
             getSupplier = Supplier().selectSupplier()
@@ -22,7 +22,7 @@ def bossDataPengiriman():
 
 @app.route('/data-pengiriman/store', methods=['POST'])
 def bossDataPengirimanStore():
-    if session['role'] == 1:
+    if session['role'] == 1 or session['role'] == 2:
         if request.method == 'POST' and 'id_barang' in request.form and 'stok' in request.form :
             id_barang = request.form.getlist('id_barang')
             stok = request.form['stok']
@@ -40,7 +40,7 @@ def bossDataPengirimanStore():
 @app.route('/data-pengiriman/detail/<int:id>', methods = ['GET'])
 def bossDataPengirimanOne(id):
     if 'loggedin' in session:
-        if session['role'] == 1 :
+        if session['role'] == 1 or session['role'] == 2:
             getPengirimanOne = Pengiriman().selectPengirimanOne(id)
 
             return render_template('menu/data_pengiriman_detail.html', dataPengirimanOne=getPengirimanOne)
@@ -53,7 +53,7 @@ def bossDataPengirimanOne(id):
 @app.route('/data-pengiriman/detail/update', methods =['POST'])
 def bossDataPengirimanUpdate():
     if 'loggedin' in session:
-        if session['role'] == 1 :
+        if session['role'] == 1 or session['role'] == 2:
             if request.method == 'POST' and 'stok' in request.form:
                 id = request.form['id']
                 stok = request.form['stok']
