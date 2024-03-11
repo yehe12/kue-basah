03c32402ecb43f512e0a1a37b5155f5f89842a28
muhammad abdullah <ma22052000@gmail.com>
2024-03-11 11:37:32 +0700

    navbar

--- app/controllers/Admin/AdminDashboadController.py
+++ app/controllers/Admin/AdminDashboadController.py
@@ -3,12 +3,14 @@ from flask import session, render_template, redirect, url_for
 
 from app.models.user import *
 
-@app.route('/admin/dashboard')
+@app.route('/admin-dashboard')
 def adminDashboard():
     if 'loggedin' in session:
         if session['role'] == 2 :
 
-            return render_template('admin/admin_dashboard.html')
+            getName = session['name']
+            
+            return render_template('admin/admin_dashboard.html', dataName = getName)
 
         else :
 
